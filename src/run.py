fdfe4d4905af123cdd4336dc39ecdce7fc2d4507
Adam Zink <adamzink55@gmail.com>
2018-03-26 20:49:08 -0400

    Write graphs and output csv file for full set of data

--- src/run.py
+++ src/run.py
@@ -1,13 +1,42 @@
 from grapher import Grapher
+from writer import Writer
 
-grapher = Grapher()
-grapher.load_sheet_from_excel('Solvent_variation_condensed_data.xlsx', 'Initial')
+# ['Initial', '24 hours', '1 week', '2 weeks', '4 weeks', '1000 hr']
+sheets = ['Initial', '24 hours', '1 week', '2 weeks', '4 weeks', '1000 hr']
 
-grapher.calc_exp_model('Wavelength', '90_10_1_Air')
-grapher.show_graph()
+# ['90_10', '75_25', '50_50']
+mixtures = ['90_10', '75_25', '50_50']
 
-grapher.calc_exp_model('Wavelength', '90_10_2_Air')
-grapher.show_graph()
+# ['N2', 'Air', 'O2']
+gases = ['N2', 'Air', 'O2']
 
-grapher.calc_exp_model('Wavelength', '90_10_3_Air')
-grapher.show_graph()
+# ['1', '2', '3']
+measurements = ['1', '2', '3']
+
+
+excel_writer = Writer()
+excel_writer.save_row('Time,Mix,Measurement,' + ','.join(gases))
+
+for sheet in sheets:
+	print('\n======== ' + sheet + ' ========')
+	grapher = Grapher()
+	grapher.load_sheet_from_excel('Solvent_variation_condensed_data.xlsx', sheet)
+
+	for mixture in mixtures:
+		for measurement in measurements:
+			resultMap = {}
+			for gas in gases:
+
+				column_name = mixture + '_' + measurement + '_' + gas
+				print('\n' + column_name)
+
+				grapher.calc_exp_model('Wavelength', column_name)
+				grapher.save_graph()
+
+				resultMap[gas] = grapher.get_subtracted_peak_intensity()
+
+			excel_writer.save_row(sheet + ',' + mixture + ',' + measurement + ',' + ','.join([str(resultMap[gas]) for gas in gases]))
+
+excel_writer.export_to_excel('subtraction_results.csv')  # , 'test_sheet'
+
+print('\nDone writing file')
